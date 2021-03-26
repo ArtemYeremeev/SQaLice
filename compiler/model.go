abbe2ea4e344f9fa9106f08fb664d0bcd900d88a
eremeev_as <eremeev_as@apteka-aprel.ru>
2021-03-26 14:26:44 +0300

    Added dinamic model handling

---
+++ compiler/model.go
@@ -0,0 +1,18 @@
+package compiler
+
+import (
+	"reflect"
+	"strings"
+)
+
+// FormDinamicModel forms a model containing fields for building query
+func FormDinamicModel(model reflect.Value) map[string]string {
+	modelTypes := model.Type()
+
+	fieldsMap := make(map[string]string, model.NumField())
+	for i := 0; i < model.NumField(); i++ { // json tag: sql tag
+		fieldsMap[strings.TrimSuffix(modelTypes.Field(i).Tag.Get("json"), ",omitempty")] = modelTypes.Field(i).Tag.Get("sql")
+	}
+
+	return fieldsMap
+}
